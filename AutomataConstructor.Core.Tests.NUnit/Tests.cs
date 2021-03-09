051f26a2e419096b655ead01ba16f8c063d9a072
Alexander-Ploskin <ploskin0107@gmail.com>
2021-03-09 16:10:36 +0300

    Create project by template

---
+++ AutomataConstructor.Core.Tests.NUnit/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace AutomataConstructor.Core.Tests.NUnit
+{
+    // TODO WTS: Add appropriate unit tests.
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ AutomataConstructor/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using AutomataConstructor.Helpers;
+
+namespace AutomataConstructor.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
