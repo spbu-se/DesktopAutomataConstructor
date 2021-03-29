d10439fe1d561607ad1f165a5069ec456cd1f11f
Alexander-Ploskin <ploskin0107@gmail.com>
2021-03-29 12:42:14 +0300

    Fix toolbar

--- ControlsLibrary/Controls/Toolbar/ToolbarViewModel.cs
+++ ControlsLibrary/Controls/Toolbar/ToolbarViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using ControlsLibrary.ViewModel.Base;
 using ControlsLibrary.Model;
-using ControlsLibrary.Infrastructure.Command;
-using System.Windows.Input;
+using System;
 
 namespace ControlsLibrary.Controls.Toolbar
 {
@@ -9,12 +8,79 @@ namespace ControlsLibrary.Controls.Toolbar
     {
         private SelectedTool selectedTool;
 
-        public SelectedTool SelectedTool { get => selectedTool; set => Set(ref selectedTool, value); }
+        public SelectedTool SelectedTool { get => selectedTool; }
 
-        
+        public delegate void SelectedToolChangedEventHandler(object sender, EventArgs e);
 
-        public ToolbarViewModel()
+        public event SelectedToolChangedEventHandler SelectedToolChanged;
+
+        private bool selectToolSelected;
+        private bool editToolSelected;
+        private bool deleteToolSelected;
+        private bool editAttributesToolSelected;
+
+        public bool SelectToolSelected
         {
+            get => selectToolSelected;
+            set
+            {
+                selectedTool = SelectedTool.Select;
+                Set(ref selectToolSelected, value);
+                if (value)
+                {
+                    EditToolSelected = false;
+                    DeleteToolSelected = false;
+                    EditAttributesToolSelected = false;
+                }
+            }
+        }
+
+        public bool EditToolSelected
+        {
+            get => editToolSelected;
+            set
+            {
+                selectedTool = SelectedTool.Edit;
+                Set(ref editToolSelected, value);
+                if (value)
+                {
+                    SelectToolSelected = false;
+                    DeleteToolSelected = false;
+                    EditAttributesToolSelected = false;
+                }
+            }
+        }
+
+        public bool DeleteToolSelected
+        {
+            get => deleteToolSelected;
+            set
+            {
+                selectedTool = SelectedTool.Delete;
+                Set(ref deleteToolSelected, value);
+                if (value)
+                {
+                    SelectToolSelected = false;
+                    EditToolSelected = false;
+                    EditAttributesToolSelected = false;
+                }
+            }
+        }
+
+        public bool EditAttributesToolSelected
+        {
+            get => editAttributesToolSelected;
+            set
+            {
+                selectedTool = SelectedTool.EditAttributes;
+                Set(ref editAttributesToolSelected, value);
+                if (value)
+                {
+                    SelectToolSelected = false;
+                    EditToolSelected = false;
+                    DeleteToolSelected = false;
+                }
+            }
         }
     }
 }
