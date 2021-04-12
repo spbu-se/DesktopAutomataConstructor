296968b7dc10541fdbc8fae9ba2b59967be5293e
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-12 18:32:47 +0300

    Protorype

---
+++ ControlsLibrary/Controls/ErrorReporter/ErrorReporterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using QuickGraph;
+using ControlsLibrary.ViewModel.Base;
+using ControlsLibrary.Model;
+using System.ComponentModel;
+
+namespace ControlsLibrary.Controls.ErrorReporter
+{
+    public class ErrorReporterViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        private BidirectionalGraph<NodeViewModel, EdgeViewModel> graph = new BidirectionalGraph<NodeViewModel, EdgeViewModel>();
+        public BidirectionalGraph<NodeViewModel, EdgeViewModel> Graph
+        {
+            get => graph;
+            set
+            {
+                Set(ref graph, value);
+            }
+        }
+    }
+}
