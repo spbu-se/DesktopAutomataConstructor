510873ba70bd06c8d6375850792f82418f95480f
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-17 17:03:25 +0300

    Create test panel control

---
+++ ControlsLibrary/Controls/TestPanel/TestPanel.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlsLibrary.Controls.TestPanel
+{
+    /// <summary>
+    /// Interaction logic for TestPanel.xaml
+    /// </summary>
+    public partial class TestPanel : UserControl
+    {
+        public TestPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ControlsLibrary/Controls/TestPanel/TestPanelViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlsLibrary.Controls.TestPanel
+{
+    class TestPanelViewModel
+    {
+    }
+}
