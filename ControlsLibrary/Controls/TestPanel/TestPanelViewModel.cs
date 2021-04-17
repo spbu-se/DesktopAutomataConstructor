1be00959a0fcbd10cb4d9c6449394218d22465d2
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-17 17:19:50 +0300

    Create test panel view model

--- ControlsLibrary/Controls/TestPanel/TestPanelViewModel.cs
+++ ControlsLibrary/Controls/TestPanel/TestPanelViewModel.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ControlsLibrary.Infrastructure.Command;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace ControlsLibrary.Controls.TestPanel
 {
-    class TestPanelViewModel
+    internal class TestPanelViewModel
     {
+        public TestPanelViewModel()
+        {
+            AddTestCommand = new RelayCommand(OnAddTestCommandExecuted, CanAddTestCommandExecute);
+        }
+
+        public ObservableCollection<TestViewModel> Tests { get; set; }
+
+        public ICommand AddTestCommand;
+
+        private bool CanAddTestCommandExecute(object p) => true;
+
+        private void OnAddTestCommandExecuted(object p) => Tests.Add(new TestViewModel());
     }
 }
