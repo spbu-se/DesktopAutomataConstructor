7a59fdfd65c7ae698d8c3796cfcb5edb0dc0448a
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-19 19:31:41 +0300

    Add hidding command

--- ControlsLibrary/Controls/TestPanel/TestPanelViewModel.cs
+++ ControlsLibrary/Controls/TestPanel/TestPanelViewModel.cs
@@ -1,17 +1,29 @@
 ﻿using ControlsLibrary.Infrastructure.Command;
+using ControlsLibrary.ViewModel.Base;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using ControlsLibrary.Model;
 
 namespace ControlsLibrary.Controls.TestPanel
 {
-    internal class TestPanelViewModel
+    internal class TestPanelViewModel : BaseViewModel
     {
         public TestPanelViewModel()
         {
             AddTestCommand = new RelayCommand(OnAddTestCommandExecuted, CanAddTestCommandExecute);
+            HideCommand = new RelayCommand(OnHideCommandExecuted, CanHideCommandExecute);
             Tests = new ObservableCollection<TestViewModel>();
         }
+
+        public ICommand HideCommand { get; }
+
+        private void OnHideCommandExecuted(object p) => isHidden = !isHidden;
+
+        private bool CanHideCommandExecute(object p) => true;
+
+        private bool isHidden = true;
+
+        public bool IsHidden { get => isHidden; set => Set(ref isHidden, value); }
 
         public ObservableCollection<TestViewModel> Tests { get; set; }
 
