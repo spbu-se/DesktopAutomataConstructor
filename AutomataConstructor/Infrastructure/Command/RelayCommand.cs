fd1b5457a2464d775e1924fbde05d1cfb3fc7766
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-22 08:58:16 +0300

    Add command infrastructure to main project

---
+++ AutomataConstructor/Infrastructure/Command/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AutomataConstructor.Infrastructure.Command
+{
+    /// <summary>
+    /// A command that can be instantiated passing the actions to be executed 
+    /// </summary>
+    public class RelayCommand : BaseCommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        /// <summary>
+        /// Basic constructor provides to set an action to execute by command and function that tells if action can be executed
+        /// </summary>
+        /// <param name="execute">Action to be executed</param>
+        /// <param name="canExecute">Function that tells if action can be executed</param>
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+
+        public override bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => execute(parameter);
+    }
+}
