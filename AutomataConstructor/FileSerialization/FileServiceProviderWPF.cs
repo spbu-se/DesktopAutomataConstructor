3100b01c7edba3cb05573bd8a7eb9fe86067c6c0
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-22 09:04:47 +0300

    Add save layout command

--- AutomataConstructor/FileSerialization/FileServiceProviderWPF.cs
+++ AutomataConstructor/FileSerialization/FileServiceProviderWPF.cs
@@ -5,65 +5,62 @@ using YAXLib;
 
 namespace AutomataConstructor.FileSerialization
 {
-    internal class FileServiceProviderWPF
+    /// <summary>
+    /// WPF implementation of file serialization and deserialization
+    /// </summary>
+    public static class FileServiceProviderWpf
     {
         /// <summary>
-        /// WPF implementation of file serialization and deserialization
+        /// Serializes data classes list to file
         /// </summary>
-        public static class FileServiceProviderWpf
+        /// <param name="filename">File name</param>
+        /// <param name="modelsList">Data classes list</param>
+        public static void SerializeDataToFile(string filename, List<GraphSerializationData> modelsList)
         {
-            /// <summary>
-            /// Serializes data classes list to file
-            /// </summary>
-            /// <param name="filename">File name</param>
-            /// <param name="modelsList">Data classes list</param>
-            public static void SerializeDataToFile(string filename, List<GraphSerializationData> modelsList)
+            using (FileStream stream = File.Open(filename, FileMode.Create, FileAccess.Write, FileShare.Read))
             {
-                using (FileStream stream = File.Open(filename, FileMode.Create, FileAccess.Write, FileShare.Read))
-                {
-                    SerializeDataToStream(stream, modelsList);
-                }
+                SerializeDataToStream(stream, modelsList);
             }
+        }
 
-            /// <summary>
-            /// Deserializes data classes list from file
-            /// </summary>
-            /// <param name="filename">File name</param>
-            public static List<GraphSerializationData> DeserializeDataFromFile(string filename)
+        /// <summary>
+        /// Deserializes data classes list from file
+        /// </summary>
+        /// <param name="filename">File name</param>
+        public static List<GraphSerializationData> DeserializeDataFromFile(string filename)
+        {
+            using (FileStream stream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
-                using (FileStream stream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
-                {
-                    return DeserializeDataFromStream(stream);
-                }
+                return DeserializeDataFromStream(stream);
             }
+        }
 
-            /// <summary>
-            /// Serializes graph data list to a stream
-            /// </summary>
-            /// <param name="stream">The destination stream</param>
-            /// <param name="modelsList">The graph data</param>
-            public static void SerializeDataToStream(Stream stream, List<GraphSerializationData> modelsList)
+        /// <summary>
+        /// Serializes graph data list to a stream
+        /// </summary>
+        /// <param name="stream">The destination stream</param>
+        /// <param name="modelsList">The graph data</param>
+        public static void SerializeDataToStream(Stream stream, List<GraphSerializationData> modelsList)
+        {
+            var serializer = new YAXSerializer(typeof(List<GraphSerializationData>));
+            using (var textWriter = new StreamWriter(stream))
             {
-                var serializer = new YAXSerializer(typeof(List<GraphSerializationData>));
-                using (var textWriter = new StreamWriter(stream))
-                {
-                    serializer.Serialize(modelsList, textWriter);
-                    textWriter.Flush();
-                }
+                serializer.Serialize(modelsList, textWriter);
+                textWriter.Flush();
             }
+        }
 
-            /// <summary>
-            /// Deserializes graph data from a stream
-            /// </summary>
-            /// <param name="stream">The stream</param>
-            /// <returns>The graph data</returns>
-            public static List<GraphSerializationData> DeserializeDataFromStream(Stream stream)
+        /// <summary>
+        /// Deserializes graph data from a stream
+        /// </summary>
+        /// <param name="stream">The stream</param>
+        /// <returns>The graph data</returns>
+        public static List<GraphSerializationData> DeserializeDataFromStream(Stream stream)
+        {
+            var deserializer = new YAXSerializer(typeof(List<GraphSerializationData>));
+            using (var textReader = new StreamReader(stream))
             {
-                var deserializer = new YAXSerializer(typeof(List<GraphSerializationData>));
-                using (var textReader = new StreamReader(stream))
-                {
-                    return (List<GraphSerializationData>)deserializer.Deserialize(textReader);
-                }
+                return (List<GraphSerializationData>)deserializer.Deserialize(textReader);
             }
         }
     }
--- AutomataConstructor/ViewModels/SaveManagerViewModel.cs
+++ AutomataConstructor/ViewModels/SaveManagerViewModel.cs
@@ -1,8 +1,33 @@
 ﻿using AutomataConstructor.FileSerialization;
+using AutomataConstructor.Infrastructure.Command;
+using ControlsLibrary.Controls.Scene;
+using Microsoft.Win32;
+using System.Windows.Input;
 
 namespace AutomataConstructor.ViewModels
 {
     internal class SaveManagerViewModel
     {
+        public SaveManagerViewModel()
+        {
+            SaveLayoutCommand = new RelayCommand(OnSaveLayoutCommandExecuted, CanSaveLayoutCommandExecute);
+        }
+
+        private GraphArea graph;
+        public GraphArea Graph { get; set; }
+
+        public ICommand SaveLayoutCommand { get; set; }
+
+        private void OnSaveLayoutCommandExecuted(object p)
+        {
+            var dlg = new SaveFileDialog { Filter = "All files|*.*", Title = "Select layout file name", FileName = "laytest.xml" };
+            if (dlg.ShowDialog() == true)
+            {
+                FileServiceProviderWpf.SerializeDataToFile(dlg.FileName, graph.ExtractSerializationData());
+            }
+        }
+
+        private bool CanSaveLayoutCommandExecute(object p) => graph.LogicCore.Graph != null && graph.VertexList.Count > 0;
+
     }
 }
