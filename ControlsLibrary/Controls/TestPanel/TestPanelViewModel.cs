2796c68e63cff0c1dbc85273818f4e3a0b2ed7ce
Alexander-Ploskin <ploskin0107@gmail.com>
2021-04-25 15:03:47 +0300

    Add run all tests command

--- ControlsLibrary/Controls/TestPanel/TestPanelViewModel.cs
+++ ControlsLibrary/Controls/TestPanel/TestPanelViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Windows.Input;
 using ControlsLibrary.Model;
 using QuickGraph;
+using System.Threading.Tasks;
 
 namespace ControlsLibrary.Controls.TestPanel
 {
@@ -13,6 +14,7 @@ namespace ControlsLibrary.Controls.TestPanel
         {
             AddTestCommand = new RelayCommand(OnAddTestCommandExecuted, CanAddTestCommandExecute);
             HideCommand = new RelayCommand(OnHideCommandExecuted, CanHideCommandExecute);
+            RunAllTestsCommand = new RelayCommand(OnRunAllTestsCommandExecuted, CanRunAllTestsCommandExecute);
             Tests = new ObservableCollection<TestViewModel>();
         }
 
@@ -40,6 +42,16 @@ namespace ControlsLibrary.Controls.TestPanel
         private bool isHidden = true;
 
         public bool IsHidden { get => isHidden; set => Set(ref isHidden, value); }
+
+        public ICommand RunAllTestsCommand { get; set; }
+
+        private void OnRunAllTestsCommandExecuted(object p)
+        {
+            Parallel.ForEach(Tests, (test) => test.ExecuteCommand.Execute(this));
+        }
+
+        private bool CanRunAllTestsCommandExecute(object p)
+            => Tests != null;
 
         public ObservableCollection<TestViewModel> Tests { get; set; }
 
