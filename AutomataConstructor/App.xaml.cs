27195030ade971a05529e73657881d9ee4fe4dce
Alexander-Ploskin <ploskin0107@gmail.com>
2021-05-03 17:20:52 +0300

    Set language from the OS settings

--- AutomataConstructor/App.xaml.cs
+++ AutomataConstructor/App.xaml.cs
@@ -14,7 +14,7 @@ namespace AutomataConstructor
             //AutomataConstructor.Properties.Settings.Default.language = "ru-RU";
             var language = Thread.CurrentThread.CurrentUICulture;
             Thread.CurrentThread.CurrentUICulture = new CultureInfo(CultureInfo.CurrentUICulture.TwoLetterISOLanguageName);
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
             base.OnStartup(e);
         }
     }
