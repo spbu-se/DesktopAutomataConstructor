1c1f565cb6b894767645ef7dc66d79dffe840e9a
Alena Kropacheva <jeykolton@gmail.com>
2021-07-20 17:06:24 +0300

    async command class added

---
+++ ControlsLibrary/Infrastructure/Command/AsyncBaseCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ControlsLibrary.Infrastructure.Command
+{
+    public abstract class AsyncBaseCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        public abstract bool CanExecute(object parameter);
+        
+        public abstract void Execute(object parameter);
+        
+        protected abstract Task ExecuteAsync(object parameter);
+    }
+}
