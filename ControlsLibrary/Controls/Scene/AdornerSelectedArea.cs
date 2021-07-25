3e18c53f9a7c957626c383b14b0ef04e0cda89af
Аня <lessness19@gmail.com>
2021-07-25 11:09:13 +0300

    Add group selection and deletion operations

---
+++ ControlsLibrary/Controls/Scene/AdornerSelectedArea.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace ControlsLibrary.Controls.Scene
+{
+    public class AdornerSelectedArea : Adorner
+    {
+        private Rect selectedRect;
+        private SolidColorBrush renderBrush;
+        private Pen renderPen;
+        public AdornerSelectedArea(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            selectedRect = new Rect(new Size(0, 0));
+            renderBrush = new SolidColorBrush(Colors.Blue);
+            renderBrush.Opacity = 0.2;
+            renderPen = new Pen(new SolidColorBrush(Colors.Navy), 1.5);
+        }
+
+        public Rect SelectedRect
+        {
+            get => selectedRect;
+            set => selectedRect = value;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            drawingContext.DrawRectangle(renderBrush, renderPen, selectedRect);
+        }
+    }
+}
+
