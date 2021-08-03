ebf92028139e5226257ba89e82780e92bbb79dfd
Ilya Muravjov <muravjovilya@gmail.com>
2021-08-04 01:07:40 +0300

    Avoid parallel edges after converting NFA to DFA

--- ControlsLibrary/Controls/Scene/Scene.xaml.cs
+++ ControlsLibrary/Controls/Scene/Scene.xaml.cs
@@ -666,11 +666,13 @@ namespace ControlsLibrary.Controls.Scene
             try
             {
                 var dfaGraph = NfaToDfaConverter.Convert(graphArea.LogicCore.Graph);
-                graphArea.VertexList.Values.ForEach(SafeRemoveVertex);
+                graphArea.RemoveAllEdges();
+                graphArea.RemoveAllVertices();
                 dfaGraph.Vertices.ForEach(node => CreateVertexControl(node));
                 dfaGraph.Edges.ForEach(edge => CreateEdgeControl(edge));
                 numberOfVertex = graphArea.VertexList.Count;
                 graphArea.RelayoutGraph();
+                graphArea.EdgesList.Values.ForEach(AvoidParallelEdges);
                 GraphEdited?.Invoke(this, EventArgs.Empty);
             }
             catch (InvalidOperationException exception)
