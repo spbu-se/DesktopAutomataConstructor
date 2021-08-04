b05c2a2d35a0cd5ab296ee68dfe40274f585cf7a
Анна <lessness19@gmail.com>
2021-08-04 22:04:45 +0300

    Add Edit- Vertex and Edge Command classes

---
+++ ControlsLibrary/Controls/Scene/Commands/EditEdgeCommand.cs
@@ -0,0 +1,50 @@
+﻿using ControlsLibrary.ViewModel;
+using System;
+using System.Collections.Generic;
+
+namespace ControlsLibrary.Controls.Scene.Commands
+{
+    class EditEdgeCommand : ISceneCommand
+    {
+        static Dictionary<string, Action<EdgeViewModel, object>> setProperty =
+            new Dictionary<string, Action<EdgeViewModel, object>>
+            {
+                {nameof(EdgeViewModel.IsEpsilon),
+                    (edgeViewModel, value) => edgeViewModel.IsEpsilon = (bool)value },
+                {nameof(EdgeViewModel.IsExpanded),
+                    (edgeViewModel, value) => edgeViewModel.IsExpanded = (bool)value },
+                {nameof(EdgeViewModel.TransitionTokensString),
+                    (edgeViewModel, value) => edgeViewModel.TransitionTokensString = (string)value }                
+            };
+
+        private readonly EdgeViewModel edgeViewModel;
+        private readonly string propertyName;
+        private readonly object oldValue;
+        private readonly object newValue;
+
+        public EditEdgeCommand(EdgeViewModel edgeViewModel, string propertyName, object oldValue, object newValue)
+        {
+            this.edgeViewModel = edgeViewModel;
+            this.propertyName = propertyName;
+            this.oldValue = oldValue;
+            this.newValue = newValue;
+        }
+
+        public bool CanBeUndone => true;
+
+        public void Execute()
+        {
+            try
+            {
+                setProperty[propertyName](edgeViewModel, newValue);
+            }
+            catch (InvalidCastException) { }
+        }
+
+        public void Undo()
+        {
+            var command = new EditEdgeCommand(edgeViewModel, propertyName, newValue, oldValue);
+            command.Execute();
+        }
+    }
+}
---
+++ ControlsLibrary/Controls/Scene/Commands/EditVertexCommand.cs
@@ -0,0 +1,51 @@
+﻿using ControlsLibrary.ViewModel;
+using System;
+using System.Collections.Generic;
+
+namespace ControlsLibrary.Controls.Scene.Commands
+{
+    class EditVertexCommand
+    {
+        static Dictionary<string, Action<NodeViewModel, object>> setProperty =
+            new Dictionary<string, Action<NodeViewModel, object>>
+            {
+                {nameof(NodeViewModel.IsInitial),
+                    (nodeViewModel, value) => nodeViewModel.IsInitial = (bool)value },
+                {nameof(NodeViewModel.IsFinal),
+                    (nodeViewModel, value) => nodeViewModel.IsFinal = (bool)value },
+                {nameof(NodeViewModel.Name),
+                    (nodeViewModel, value) => nodeViewModel.Name = (string)value }
+            };
+
+        private readonly NodeViewModel nodeViewModel;
+        private readonly string propertyName;
+        private readonly object oldValue;
+        private readonly object newValue;
+
+        public EditVertexCommand(NodeViewModel nodeViewModel, string propertyName, object oldValue, object newValue)
+        {
+            this.nodeViewModel = nodeViewModel;
+            this.propertyName = propertyName;
+            this.oldValue = oldValue;
+            this.newValue = newValue;
+        }
+
+        public bool CanBeUndone => true;
+
+        public void Execute()
+        {
+            try
+            {
+                setProperty[propertyName](nodeViewModel, newValue);
+            }
+            catch (InvalidCastException) { }
+        }
+
+        public void Undo()
+        {
+            var command = new EditVertexCommand(nodeViewModel, propertyName, newValue, oldValue);
+            command.Execute();
+        }
+    }
+}
+
